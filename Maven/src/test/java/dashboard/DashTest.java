b5f7ec30db9c37dc63cb0332d001a35f804b6de5
Kanaka rao <Kanaka rao@LAPTOP-PDMKP857>
2018-06-25 12:45:06 +0530

    Maven Project

---
+++ Maven/src/test/java/dashboard/DashTest.java
@@ -0,0 +1,19 @@
+package dashboard;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterTest;
+
+public class DashTest {
+  @Test
+  public void f() {
+  }
+  @BeforeClass
+  public void beforeClass() {
+  }
+
+  @AfterTest
+  public void afterTest() {
+  }
+
+}
